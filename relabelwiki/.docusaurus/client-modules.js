67504a1777a00534c6e4022cedb4e6f9add3b9f3
ProjectReLabel <asheroftherealm@gmail.com>
2025-03-01 16:37:07 +0000

    build wiki

---
+++ relabelwiki/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\olida\\Documents\\GitHub\\projectrelabel.github.io\\relabelwiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\olida\\Documents\\GitHub\\projectrelabel.github.io\\relabelwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\olida\\Documents\\GitHub\\projectrelabel.github.io\\relabelwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\olida\\Documents\\GitHub\\projectrelabel.github.io\\relabelwiki\\src\\css\\custom.css"),
+];
